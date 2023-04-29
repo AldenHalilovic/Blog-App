261ae91975b87cdc0915ba60bd9652271ddd91a9
Alden Halilovic <alden.halilovic@dobi.mk>
2023-04-29 16:19:17 +0200

    [ADDED] Sign in Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import Layout from '@/Components/Layout/Layout'
 import '@/styles/globals.css'
+import Login from "../Components/login/loginFrom"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<Login><Layout>
+  <Component {...pageProps} />
+  </Layout>
+  </Login>
+  
+ 
 }
